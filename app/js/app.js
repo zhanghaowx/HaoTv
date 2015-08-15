0c24c4f059c15be0506299b294ff2d0cad853856
Hao Zhang <zhanghaowx@gmail.com>
2015-08-14 23:12:35 -0500

    Separate siderbar and footer from index

--- app/js/app.js
+++ app/js/app.js
@@ -26,11 +26,26 @@ define([
             redirectTo: '/dashboard'
         });
     }]);
-    // UI components: Header, Footer, Sidebar, ... , etc.
+    /*
+     * Layout Partials.
+     * By default the partials are loaded through AngularJS ng-include directive. In case they loaded in server side(e.g: PHP include function) then below partial
+     * initialization can be disabled and Layout.init() should be called on page load complete as explained above.
+     */
     app.controller('HeaderController', ['$scope', function ($scope) {
         $scope.$on('$includeContentLoaded', function () {
-            Layout.init(); // init header
+            //Layout.initHeader(); // init header
         });
     }]);
+    app.controller('FooterController', ['$scope', function ($scope) {
+        $scope.$on('$includeContentLoaded', function () {
+            //Layout.initFooter(); // init footer
+        });
+    }]);
+    app.controller('SidebarController', ['$scope', function ($scope) {
+        $scope.$on('$includeContentLoaded', function () {
+            //Layout.initSidebar(); // init sidebar
+        });
+    }]);
+
     return app;
 });
